d99640c97ecb584f92b7798f1d092ea0e3c71e71
Felipe Lima <felipe.lima@gmail.com>
2012-02-02 00:32:57 -0800

    ignoring node_modules

--- app.js
+++ app.js
@@ -49,5 +49,5 @@ app.get('/', function(req, res) {
   }, 200);
 });
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
