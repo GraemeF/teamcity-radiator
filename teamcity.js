9e385e6ed37c776f42be61a15e451a2dbd39f0bd
Graeme Foster <graemef@gmail.com>
2012-04-26 13:10:22 +0100

    Use npm to get the TeamCity server config.

--- teamcity.js
+++ teamcity.js
@@ -6,13 +6,13 @@ var libxmljs = require("libxmljs");
 var http = require("http");
 
 exports.requestXml = function(path, callback) {
-  var base_url = process.env.RADIATOR_TEAMCITY_HOST;
+  var base_url = process.env.npm_package_config_teamcity_host;
   var client = http.createClient(80, base_url);
-  var username = process.env.RADIATOR_TEAMCITY_USERNAME;
-  var password = process.env.RADIATOR_TEAMCITY_PASSWORD;
+  var username = process.env.npm_package_config_teamcity_username;
+  var password = process.env.npm_package_config_teamcity_password;
   var auth = 'Basic ' + new Buffer(username + ':' + password).toString('base64');
   
-  var request = client.request('GET', "/httpAuth/app/rest" + path, {'host': 'ci.gogobot.com', 'Authorization': auth});
+  var request = client.request('GET', "/httpAuth/app/rest" + path, {'host': base_url, 'Authorization': auth});
 
   request.end();
 
